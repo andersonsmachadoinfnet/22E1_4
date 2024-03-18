905bba873d26ac13dfc4ab3105a2075703647f6a
unknown <anderson.smachado@al.infnet.edu.br>
2024-03-17 22:09:28 -0300

    Adiciona o projeto do TP3, ASP.NET Core MVC

---
+++ TP3/GestaoDeLaboratorios/DAL/INFNETContexto.cs
@@ -0,0 +1,15 @@
+﻿using GestaoDeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoDeLaboratorios.DAL
+{
+    public class INFNETContexto : DbContext
+    {
+        public INFNETContexto(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Computadores> Computadores { get; set; }
+    }
+}
---
+++ TP3/GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required] 
+        public string Marca { get; set; }
+        
+        [Required] 
+        public string Processador { get; set; }
+        
+        [Required] 
+        public string PlacaMae { get; set; }
+        
+        [Required] 
+        public int Memoria { get; set; }
+        
+        [Required] 
+        public int HD { get; set; }
+        
+        [Required] 
+        public int NumeroPatrimonio { get; set; }
+        
+        [Required] 
+        public DateOnly DataCompra { get; set; }
+    }
+}
